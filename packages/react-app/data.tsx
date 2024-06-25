d969cb506d77e694f461353550ee0ceeb3b4a063
alvienzo720 <mutebialvinalvienzo@gmail.com>
2024-06-25 14:22:53 +0300

    Remove Photos

--- packages/react-app/data.tsx
+++ packages/react-app/data.tsx
@@ -1,8 +1,8 @@
 export const SERVICES = [
-    {name: 'uber', img: '/images/uber.png', price: 30},
-    {name: 'kfc', img: '/images/kfclogo.png', price: 10},
-    {name: 'safe boda', img: '/images/safeboda.png', price: 5},
-    {name: 'cafe javas', img: '/images/cjs.png', price: 20},
-    {name: 'faras', img: '/images/faras.png', price: 5},
-    {name: 'bolt', img: '/images/bolt.png', price: 25},
-]
+  { name: "meat guy", img: "/images/meatguy.jpg", price: 30 },
+  { name: "maisaha cakes", img: "/images/cakes.png", price: 10 },
+  { name: "nkwanzi scents", img: "/images/nkwanzi.png", price: 5 },
+  { name: "bunny", img: "/images/bunny.png", price: 20 },
+  { name: "sole connection", img: "/images/sole.png", price: 5 },
+  { name: "hair by hawah", img: "/images/hairby.jpg", price: 25 },
+];
