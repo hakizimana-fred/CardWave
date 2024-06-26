9b95d66cd7d57ef2a3afff902d2e35440f8afa4b
alvienzo720 <mutebialvinalvienzo@gmail.com>
2024-06-26 20:54:27 +0300

    Modify Maisha Image

--- packages/react-app/data.tsx
+++ packages/react-app/data.tsx
@@ -1,7 +1,7 @@
 export const SERVICES = [
   { name: "Bunny", img: "/images/bunny.png", price: 30 },
   { name: "haiwan", img: "/images/haiwan.jpg", price: 10 },
-  { name: "Maisha", img: "/images/maisha.png", price: 5 },
+  { name: "Maisha", img: "/images/cakes.png", price: 5 },
   { name: "Meat Guy", img: "/images/meatguy.jpg", price: 20 },
   { name: "Sole Collection", img: "/images/sole.png", price: 25 },
   { name: "Nkwanzi", img: "/images/nkwanzi.png", price: 5 },
